a9f75a537790d5bf3ac3f0f727f2dfe86de8b993
Nicolas Cordoba <niccord33@gmail.com>
2026-03-09 12:27:39 -0500

    Add initial implementation of Depth First Search algorithm

---
+++ algorithms/depth_first_search.py
@@ -0,0 +1,7 @@
+import time
+from algorithms import Algorithm
+from utils.result import Result
+
+class DFS(Algorithm):
+  def solve(self) -> Result:
+    print("aqui va el algoritmo para resolver")
